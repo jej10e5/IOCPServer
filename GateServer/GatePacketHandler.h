766d4551e1f0e53b4d8074f98cae42277b3a43b1
jej10e5 <jej10e5@gamail.com>
2025-08-17 13:09:11 +0900

    빌드 오류 수정

--- GateServer/GatePacketHandler.h
+++ GateServer/GatePacketHandler.h
@@ -8,5 +8,6 @@ class GatePacketHandler
 public:
 	static void SendToGame(Session* _pSession, PacketHeader* _pHeader);
 	static void Handle_Eco(Session* _pSession, const char* _pData, UINT16 _ui16size);
-}
+};
 
+ 
